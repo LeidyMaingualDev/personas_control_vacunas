4ac880b4e04d583c14679a092fb467a5196e04af
LeidyMaingualDev <vanesamaingual2008@gmail.com>
2025-09-11 23:14:26 -0500

    Proyecto inicial Spring Boot reto vacunas

---
+++ src/main/java/com/vacunas/personasvulnerables/PersonasvulnerablesApplication.java
@@ -0,0 +1,13 @@
+package com.vacunas.personasvulnerables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonasvulnerablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonasvulnerablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vacunas/personasvulnerables/PersonasvulnerablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vacunas.personasvulnerables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonasvulnerablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
